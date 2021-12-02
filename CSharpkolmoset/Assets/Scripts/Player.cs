b9d7788a8623a68d294f41757b85c2f7550db714
KoskiPlayer <85992435+KoskiPlayer@users.noreply.github.com>
2021-12-02 11:07:25 +0200

    1tunti

--- CSharpkolmoset/Assets/Scripts/Player.cs
+++ CSharpkolmoset/Assets/Scripts/Player.cs
@@ -4,18 +4,67 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        // Change the color of the gameobject
         if (Input.GetKeyDown(KeyCode.G))
         {
             GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+
+        // Lets move the gameobject
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            rb.AddForce(Vector3.up * 500f);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.velocity = Vector3.forward * 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.velocity = Vector3.back * 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = Vector3.right * 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = Vector3.left * 10f;
+        }
+
+
+    } // Update funktio p‰‰ttyy t‰ss‰
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Collectible")
+        {
+            // Let¥s collect things
+            Destroy(collision.gameObject);
         }
     }
 }
